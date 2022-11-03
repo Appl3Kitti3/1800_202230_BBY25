84c00d78da44de47302955500c35f536a0e56aa6
Teddy Dumam-Ag <teddallama@gmail.com>
2022-11-03 10:51:48 -0700

    Unignored API Key

---
+++ scripts/firebaseAPI_TEAM25.js
@@ -0,0 +1,17 @@
+//----------------------------------------
+//  Your web app's Firebase configuration
+//----------------------------------------
+const firebaseConfig = {
+    apiKey: "AIzaSyBYGkV1b1RA6WZEBbraNgeMFOs5psZqbx8",
+    authDomain: "moodup-e8744.firebaseapp.com",
+    projectId: "moodup-e8744",
+    storageBucket: "moodup-e8744.appspot.com",
+    messagingSenderId: "283918345875",
+    appId: "1:283918345875:web:5438cf58c658a314234f66"
+  };
+//--------------------------------------------
+// initialize the Firebase app
+// initialize Firestore database if using it
+//--------------------------------------------
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
